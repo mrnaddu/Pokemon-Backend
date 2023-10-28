f581716e4f1fe1e15635969d8247e789206b3e8b
Mohammed Nadeem <Mohammed.Nadeem@raasoft.com>
2023-10-28 00:53:27 -0700

    updated the changes

--- Pokemon-WebApi/Models/Dtos/CreateUpdatePokemonDto.cs
+++ Pokemon-WebApi/Models/Dtos/CreateUpdatePokemonDto.cs
@@ -1,7 +1,15 @@
-﻿namespace Pokemon_WebApi.Models.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Pokemon_WebApi.Models.Dtos;
 
 public class CreateUpdatePokemonDto
 {
+    [Required]
+    [AllowNull]
     public string PokemonName { get; set; }
+    [AllowNull]
     public string PokemonImage { get; set; }
+    [AllowNull]
+    public IFormFile ImageFile { get; set; }
 }
--- Pokemon-WebApi/Repositories/Abastract/IPokemonRepository.cs
+++ Pokemon-WebApi/Repositories/Abastract/IPokemonRepository.cs
@@ -1,4 +1,6 @@
-﻿using Pokemon_WebApi.Models.Entities;
+﻿using Pokemon_WebApi.Models.Dtos;
+using Pokemon_WebApi.Models.Entities;
+using Pokemon_WebApi.Models.Response;
 
 namespace Pokemon_WebApi.Repository.Abastract;
 
